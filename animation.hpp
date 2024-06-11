e9fd6dcde5e5b9f6f8d7284e9b8a619a21ecff1e
GeraMG1105 <160290048+GeraMG1105@users.noreply.github.com>
2024-06-10 20:27:01 -0600

    clase de animacion

---
+++ animation.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Animation
+{
+public:
+    Animation(sf::Texture* texture, sf::Vector2u imageCount, float switchTime );
+    ~Animation();
+
+    void Update(int row, float deltaTime);
+
+public:
+    sf::IntRect uvRect;
+
+private:
+    sf::Vector2u imageCount;
+    sf::Vector2u currentImage;
+    
+    float totalTime;
+    float switchTime;
+
+};
+
+//Declaracion de Metodos
+
+// Metodo Constructor
+Animation::Animation(sf::Texture* texture, sf::Vector2u imageCount, float switchTime)
+{
+    this->imageCount = imageCount;
+    this->switchTime = switchTime;
+    totalTime = 0.0f;
+    currentImage.x = 0;
+   
+
+    uvRect.width = texture->getSize().x / float(imageCount.x);
+    uvRect.height = texture->getSize().y / float(imageCount.y);
+}
+// Metodo Destructor
+Animation::~Animation() {}
+
+// Metodo Update
+void Animation::Update(int row, float deltaTime)
+{
+    currentImage.y = row;
+    totalTime += deltaTime;
+
+    if (totalTime >= switchTime)
+    {
+        totalTime -= switchTime;
+        currentImage.x++;
+
+        if (currentImage.x >= imageCount.x)
+        {
+            currentImage.x = 0;
+        }
+    }
+
+    uvRect.left = currentImage.x * uvRect.width;
+    uvRect.top = currentImage.y * uvRect.height;
+}
+    
