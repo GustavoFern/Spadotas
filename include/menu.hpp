0ec2523156c0514e4e9f6d41ef49008acda457c2
GeraMG1105 <160290048+GeraMG1105@users.noreply.github.com>
2024-06-18 21:53:55 +0000

    + title and update controls image

--- include/menu.hpp
+++ include/menu.hpp
@@ -60,6 +60,7 @@ public:
         Button optionsButton(sf::Vector2f(280, 60), sf::Vector2f(500, 435), &optionsButtonTexture);
         Button exitButton(sf::Vector2f(280, 60), sf::Vector2f(500, 500), &exitTexture);
         Button backButton(sf::Vector2f(700, 130), sf::Vector2f(500, 100), &exitTexture);
+        Button title(sf::Vector2f(1280, 288),sf::Vector2f(500, 150), &titleTexture);
 
         bool inWindow = false; // Add a flag to control the controls window
 
@@ -78,8 +79,8 @@ public:
             sf::RectangleShape background(sf::Vector2f(1280, 720));
             background.setTexture(&menuBackground);
             menuWindow->draw(background);
-
             // Dibujar botones
+            title.Draw(*menuWindow);
             playButton.Draw(*menuWindow);
             optionsButton.Draw(*menuWindow);
             exitButton.Draw(*menuWindow);
